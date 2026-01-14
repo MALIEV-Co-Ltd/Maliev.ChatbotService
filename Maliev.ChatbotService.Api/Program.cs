82eb59c46cb67e1374e9b59cdf831c4df33c0ecf
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-14 18:20:47 +0700

    chore: cleanup bootstrap logging and suppress CA1848 consistently

--- Maliev.ChatbotService.Api/Program.cs
+++ Maliev.ChatbotService.Api/Program.cs
@@ -1,3 +1,4 @@
+#pragma warning disable CA1848 // For improved performance, use the LoggerMessage delegates
 using Maliev.Aspire.ServiceDefaults;
 using Maliev.ChatbotService.Application.Handlers;
 using Maliev.ChatbotService.Application.Interfaces;
